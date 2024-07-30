a7e9433a1f1b8e577950bb5b3334025871246a2e
Anshuman Anand <anshuman13069@gmail.com>
2024-07-30 04:53:56 +0000

    datatypes

---
+++ .github/datatypes.js
@@ -0,0 +1,18 @@
+// We have 7 data types in Javascript. Number, String, Boolean, BigInt, null, undefined & symbol.
+
+let myName = "Anshuman"
+console.log(typeof(myName))
+// This is a string type data.
+
+let myDob = 16021999
+console.log(typeof(myDob))
+//  This is a Number datatype.
+
+// If we assign a null value to a variable, the datatype comes out to be as OBJECT.
+// Whereas if we do not assign any value to the variable, the datatype comes out to be undefined.
+
+let ab = null
+console.log(typeof(ab))
+
+let cd
+console.log(typeof(cd))
