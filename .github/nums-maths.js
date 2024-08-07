a1a67296e7287c0211009ba0a8784c02e416db16
Anshuman Anand <anshuman13069@gmail.com>
2024-08-07 10:38:45 +0000

    numbers-maths

---
+++ .github/nums-maths.js
@@ -0,0 +1,49 @@
+// const score = 100
+// console.log(typeof(score));
+
+// const balance = new Number(100)
+// console.log(typeof(balance));
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4))
+
+// const decimalNumber = 1234.567
+// console.log(decimalNumber.toFixed(1));
+// console.log(decimalNumber.toPrecision(5));
+
+// bigNumber = 10000002220
+// console.log(bigNumber.toLocaleString('en-IN'));
+
+// For more methods and functions, read MDN docs and practice here.
+
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ \\
+
+
+
+// Maths library comes by default with JAVASCRIPT.
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.max(2,4,9,5,3));
+// console.log(Math.min(1,0,-3,-9,3,5));
+// console.log(Math.round(4.3454));
+// console.log(Math.floor(4.878));
+// console.log(Math.ceil(4.844));
+
+
+
+// console.log(Math.random()); >>>> Provides us a random number between 0 and 1.
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); 
+
+
+
+
