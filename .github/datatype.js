761def76a9a8fbc7162df279a010031e12aded34
Anshuman Anand <anshuman13069@gmail.com>
2024-08-07 03:22:53 +0000

    stack-heap-memory

--- .github/datatype.js
+++ .github/datatype.js
@@ -3,42 +3,42 @@
 
 // These are: objects, functions and arrays.
 
-let myFunction = function(){
-    console.log("Hello !!")
-}
-console.log(myFunction);
-console.log(typeof(myFunction));
+// let myFunction = function(){
+//     console.log("Hello !!")
+// }
+// console.log(myFunction);
+// console.log(typeof(myFunction));
 
-const arrFunction = ['3', '4', 'Anshuman']
-console.log(typeof(arrFunction));
+// const arrFunction = ['3', '4', 'Anshuman']
+// console.log(typeof(arrFunction));
 
-let myObj = {
-    "name": "Anshuman",
-    "Age": 25,
+// let myObj = {
+//     "name": "Anshuman",
+//     "Age": 25,
 
-}
-console.log(myObj);
-console.log(typeof(myObj));
+// }
+// console.log(myObj);
+// console.log(typeof(myObj));
 
-let id = Symbol(123)
-let anotherId = Symbol(123)
+// let id = Symbol(123)
+// let anotherId = Symbol(123)
 
-console.log(id)
-console.log(typeof(id));
+// console.log(id)
+// console.log(typeof(id));
 
-console.log(anotherId)
-console.log(typeof(anotherId));
+// console.log(anotherId)
+// console.log(typeof(anotherId));
 
-let uniqueChar = Symbol('*&^%$#@1345')
-console.log(uniqueChar);
+// let uniqueChar = Symbol('*&^%$#@1345')
+// console.log(uniqueChar);
 
 // Symbol data type is immutable and unique and can be used as identifiers for object properties.
 
-const mySymbol = Symbol('uniqueKey');
-const obj = {
-    [mySymbol]: 'value'
-};
-console.log(obj[mySymbol]); // 'value'
+// const mySymbol = Symbol('uniqueKey');
+// const obj = {
+//     [mySymbol]: 'value'
+// };
+// console.log(obj[mySymbol]); // 'value'
 /*
 Why We Need Symbols
 Avoiding Property Name Collisions:
@@ -59,3 +59,37 @@ Symbols can be used to define custom behavior for objects, such as custom iterat
 // The data type of our object, it is OBJECT.>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>            
 // When we check type of our function, it is Function.                                                
 // When we check type of arrFunction, which is an array, It comes out to be OBJECT.
+
+
+
+// All primitive datatypes of javascript are stored in STACK memory whereas all reference datatypes are stored in HEAP memory.
+
+// let myName = "Anshuman" : (Primitive datatype, hence will be stored in stack)
+// anotherName = myName  : ()
+// anotherName = "Anand"
+
+// What happens is that new value is stored in variable anotherName, but since it is stored in stack memory,
+// a reference of variable copy is provided when changing the value. Hence the value of myName remains same but anotherName changes.
+
+
+// let userOne = {
+//     user: "anshuman123",
+//     upi: "ansh@ybl"
+// }
+
+//  userOne is an object, i.e. a reference data type. Hence value is stored in Heap Memory.
+// let userTwo = {
+//     user: "anand",
+//     upi: "ansh@ybl"
+// }
+
+// userTwo.user = "anshuman123"
+
+// console.log(userOne.user);
+// console.log(userTwo.user);
+
+// We see the value of user in userTwo changes, the original value is being given as reference in Heap memory.
+// Hence, the change is being done in the original value.
+
+
+
