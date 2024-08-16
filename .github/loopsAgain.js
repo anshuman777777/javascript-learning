68e8e0f0b1ae488cf2b31805f0f3fa8cfdf7212e
Anshuman Anand <anshuman13069@gmail.com>
2024-08-16 10:50:27 +0000

    for-of and maps

---
+++ .github/loopsAgain.js
@@ -0,0 +1,28 @@
+// we will learn more array specific loops in this section.
+
+// 1 :> for of loop:
+
+
+const charArray = ["a", "b", "c", "d"]
+for (const char of charArray) {
+    // console.log(char);
+    
+    
+}
+
+
+const numeric = [2, 3, 4, 5, 6]
+for (const num of numeric) {
+    // console.log(num);
+    
+    
+}
+
+
+// MAPS: >> it is an OBJECT that holds KEY:VALUE pairs and remembers the original INSERTION ORDER of the keys
+
+const map = new Map()
+map.set('IN', 'INDIA')
+map.set('USA', 'UNITED STATES OF AMERICA')
+map.set('FR', 'FRANCE')
+console.log(map);
