3095e7c5301b00c7a8bd50153734ce543a53f02f
Anshuman Anand <anshuman13069@gmail.com>
2024-08-16 05:13:57 +0000

    while-dowhile-loops

---
+++ .github/moreLoops.js
@@ -0,0 +1,51 @@
+// WE WILL LEARN WHILE AND DO WHILE LOOPS :
+
+
+// let myNum = 0
+// while (myNum <= 10) {
+//     console.log(`Value of myNum is ${myNum}`);
+//     myNum = myNum + 2
+    
+// }
+
+
+// USING WHILE LOOP ON AN ARRAY:
+
+// myArray = ["Ayush", "Anshuman", "Aman"]
+// index = 0;
+// while (index < myArray.length) {
+//     console.log(`${myArray[index]}'s index is ${index} `);
+//     index ++
+    
+// }
+
+// NOTE: WHENEVER COMPARING WITH THE LENGTH OF AN ARRAY, WE DO NOT USE "=" , INSTEAD WE ALWAYS USE "<". E.G: index < myArray.length and not index <= myArray.length
+
+// let myArray = ["apple", "banana", "orange", "strawberry"]
+// let index = 0;
+// while(index < myArray.length){
+//     console.log(`Loop runs for ${myArray[index]}`);
+//     index ++
+    
+// }
+
+
+
+
+// In the world of programming, we often refer to loops using two different control structures.
+//  The 'for' and 'while' loops are what we call 'entry control loops' because they check the condition before entering the loop.
+//  On the other hand, 'do-while' loops are known as 'exit control loops' since they ensure that the loop body is executed at least once before checking the condition.
+
+
+
+
+
+// DO WHILE STATEMENT:
+
+// let i = 1;
+// do {
+//     console.log("Do this operation");
+//     i += 2
+
+    
+// } while (i <= 5);
